947316be26b1dd5c2d7d7acbbaf2841bab4d6306
rudream <rudream12@gmail.com>
2020-01-11 05:36:25 -0500

    added movie selection and summary rendering

--- scripts/app.js
+++ scripts/app.js
@@ -34,6 +34,11 @@ const resultsWrapper = document.querySelector(".results");
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
 
+    if (!movies.length) {
+        dropdown.classList.remove("is-actve");
+        return;
+    }
+
     resultsWrapper.innerHTML = "";
     dropdown.classList.add("is-active");
     for (let movie of movies) {
@@ -45,8 +50,49 @@ const onInput = async event => {
         ${movie.Title}
         `;
 
+        option.addEventListener("click", () => {
+            dropdown.classList.remove("is-active");
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        });
+
         resultsWrapper.appendChild(option);
     }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", event => {
+    if (!autocomplete.contains(event.target)) {
+        dropdown.classList.remove("is-active");
+    }
+});
+
+const onMovieSelect = async movie => {
+    const response = await axios.get("http://www.omdbapi.com", {
+        params: {
+            apikey: apiKey,
+            i: movie.imdbID
+        }
+    });
+    document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetails => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetails.Poster}">
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetails.Title}</h1>
+                    <h4>${movieDetails.Genre}</h4>
+                    <p>${movieDetails.Plot}</p>
+                </div>
+            </div>
+        </article>
+    `;
+};
